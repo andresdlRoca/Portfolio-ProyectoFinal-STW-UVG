c851793c73764529aceba95764daba4b9a691e78
Andresdlroca <andresdelaroca11@gmail.com>
2024-10-14 14:46:17 -0600

    Fixed bug on import intropage

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import "./styles/index.css"
 import ReactDOM from "react-dom"
 import PageWrap from "./pages/PageWrap"
-import IntroPage from "./pages/IntroPage"
+import IntroPage from "./pages/introPage"
 
 ReactDOM.render(
 	<>
